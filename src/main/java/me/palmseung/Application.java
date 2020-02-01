5feb2a89c53b39e356fd0e43c52b5d847a1f7431
LeeSeungHee <palmseung@gmail.com>
2020-02-01 20:57:18 +0900

    개발환경 변경 및 메인메서드 생성

---
+++ src/main/java/me/palmseung/Application.java
@@ -0,0 +1,10 @@
+package me.palmseung;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
